72bc95452c498ce1aa86096bd85df67b64335d8f
n-tanimoto <57885202+n-tanimoto@users.noreply.github.com>
2023-09-26 14:10:32 +0900

    Delete java-example directory

--- java-example/src/main/java/com/caphyon/codegold/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.caphyon.codegold.utils;
-
-public class Constants {
-    public final static String LOCAL = "local";
-    public final static String DEVELOPMENT = "development";
-    public final static String ONLINE = "online";
-    public final static String TEST = "test";
-}
